ab774a580adc34accb0d88a61b23d5dd1b558969
DenisNikiti <den.hikitin@gmail.com>
2024-02-04 06:51:34 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="__blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="__blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
