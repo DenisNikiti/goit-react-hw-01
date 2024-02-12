31864a49037bbdcc3c788141499f45517915adb8
DenisNikiti <den.hikitin@gmail.com>
2024-02-12 17:10:28 +0300

    7

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -5,7 +5,7 @@ import css from "./TransactionHistory.module.css";
 function TransactionHistory({ history }) {
   return (
     <table>
-      <thead>
+      <thead className={css.table}>
         <tr>
           <th className={css.tablehitght}>Type</th>
           <th className={css.tablehitght}>Amount</th>
